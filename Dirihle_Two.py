aa776fcfd34daea168d0c078aa965e7d3c79727c
Ufifus <95492474+Ufifus@users.noreply.github.com>
2022-01-22 17:12:58 +0300

    Forth

--- Dirihle_Two.py
+++ Dirihle_Two.py
@@ -116,6 +116,35 @@ class Dirihle_sub:
         return L, D, Rm
 
 
+    """Нахождение обратной матрицы"""
+
+
+    def inverse_matrix(self, matrix_origin):
+        m = np.hstack((matrix_origin,
+                       np.matrix(np.diag([1.0 for i in range(matrix_origin.shape[0])]))))
+        n = matrix_origin.shape[0]
+
+        for k in range(n):
+            swap_row = self.pick_nonzero_row(m, k)
+            if swap_row != k:
+                m[k, :], m[swap_row, :] = m[swap_row, :], np.copy(m[k, :])
+            if m[k, k] != 1:
+                m[k, :] *= 1 / m[k, k]
+            for row in range(k + 1, n):
+                m[row, :] -= m[k, :] * m[row, k]
+
+        for k in range(n - 1, 0, -1):
+            for row in range(k - 1, -1, -1):
+                if m[row, k]:
+                    m[row, :] -= m[k, :] * m[row, k]
+        return m[:n, n:]
+
+    def pick_nonzero_row(self, m, k):
+        while k < m.shape[0] and not m[k, k]:
+            k += 1
+        return k
+
+
     """Строим график"""
 
 
